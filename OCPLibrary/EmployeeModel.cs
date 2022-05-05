b0d6d34ecb1eb9fbf5fa9f2398363992cf47be79
aaronmdenny <aaronmdenny@outlook.com>
2022-05-04 20:20:55 -0400

    Add project files.

---
+++ OCPLibrary/EmployeeModel.cs
@@ -0,0 +1,11 @@
+﻿namespace OCPLibrary
+{
+    public class EmployeeModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EmailAddress { get; set; }
+    }
+}
