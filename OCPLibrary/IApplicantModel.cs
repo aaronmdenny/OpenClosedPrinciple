91667f4d3a3ca29cdd1ba4277835287699794525
aaronmdenny <aaronmdenny@outlook.com>
2022-06-04 09:36:20 -0400

    add AccountProcessor property to person model

--- OCPLibrary/IApplicantModel.cs
+++ OCPLibrary/IApplicantModel.cs
@@ -3,6 +3,9 @@
     public interface IApplicantModel
     {
         string FirstName { get; set; }
+
         string LastName { get; set; }
+
+        IAccounts AccountProcessor { get; set; }
     }
 }
--- OCPLibrary/PersonModel.cs
+++ OCPLibrary/PersonModel.cs
@@ -5,5 +5,7 @@
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
+
+        public IAccounts AccountProcessor { get; set; } = new Accounts();
     }
 }
